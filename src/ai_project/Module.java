99fc37f6cdd49efc2786d1bdeb538cea89ce523c
RidaNazir4086 <ridanazir4086@gmail.com>
2020-03-29 23:52:02 +0500

    first commit

---
+++ src/ai_project/Module.java
@@ -0,0 +1,39 @@
+package ai_project;
+
+/**
+ * Simple course module abstraction, which defines the Professors teaching the module.
+ */
+public class Module {
+    private final int moduleId;
+    private final String moduleCode;
+    private final String module;
+    private final int professorIds[];
+    
+    
+    public Module(int moduleId, String moduleCode, String module, int professorIds[]){
+        this.moduleId = moduleId;
+        this.moduleCode = moduleCode;
+        this.module = module;
+        this.professorIds = professorIds;
+    }
+    
+    public int getModuleId(){
+        return this.moduleId;
+    }
+    
+    
+    public String getModuleCode(){
+        return this.moduleCode;
+    }
+    
+    
+    public String getModuleName(){
+        return this.module;
+    }
+    
+    
+    public int getRandomProfessorId(){
+        int professorId = professorIds[(int) (professorIds.length * Math.random())];
+        return professorId;
+    }
+}
---
+++ src/ai_project/Professor.java
@@ -0,0 +1,20 @@
+package ai_project;
+
+public class Professor {
+    private final int professorId;
+    private final String professorName;
+
+    public Professor(int professorId, String professorName){
+        this.professorId = professorId;
+        this.professorName = professorName;
+    }
+    
+
+    public int getProfessorId(){
+        return this.professorId;
+    }
+    
+    public String getProfessorName(){
+        return this.professorName;
+    }
+}
